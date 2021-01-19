cc6a762d9df69eb3963de79713727548b49401aa
nisingizwe joselyne <nesjoselyne@gmail.com>
2021-01-19 12:36:13 -0800

    adding

--- home/models.py
+++ home/models.py
@@ -6,23 +6,22 @@ class Farmers(models.Model):
     names = models.CharField(max_length=255)
     idnumber = models.CharField(max_length=255)
     telephone = models.CharField(max_length=255)
-    harvesting = models.CharField(max_length=255)
     description = models.TextField()
     def __str__(self):
         return self.names
 class Cooperatives(models.Model):
     name= models.CharField(max_length=40)
-    leader = models. CharField(max_length=40)
-    harvesting_type = models.IntegerField()
+    leader = models.CharField(max_length=40)
+    harvesting_type = models.CharField(max_length=45)
     location = models.CharField(max_length=45)
     farmers = models.CharField(max_length=45)
     description = models.TextField()
     def __str__(self):
         return self.name
 class Record(models.Model):
-    date  = models.DateField()
-    cooperative = models.CharField(max_length=90)
-    farmer = models.CharField(max_length=40)
+    date  = models.DateTimeField(auto_now_add=True)
+    cooperative = models.ForeignKey( Cooperatives, on_delete=models.CASCADE)
+    farmer = models.ForeignKey( Farmers, on_delete=models.CASCADE)
     Quantity = models.CharField(max_length=40)
     phone = models.CharField(max_length=45)
    
